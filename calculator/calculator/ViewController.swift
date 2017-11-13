efe99838c4805a8256a126666fbe3d9aefac0308
Geeku <ggongpei@gmail.com>
2017-11-13 18:03:34 +0800

    layout

--- calculator/calculator/ViewController.swift
+++ calculator/calculator/ViewController.swift
@@ -10,16 +10,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    @IBOutlet weak var resultDisplay: UILabel!
+    
+    @IBAction func numClick(_ sender: UIButton) {
+        resultDisplay.text = "click"
+        print("click")
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
 
